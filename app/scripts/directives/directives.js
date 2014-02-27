5291cf4c060c1cf04f3a7fc322769924fb5808cb
Derrick Roccka <derrickroccka@gmail.com>
2014-02-27 23:47:37 +0100

    directives.js improved a little bit

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -8,23 +8,10 @@ angular.module('Directives', [])
 			num: '&px'
 		},
 		link: function(scope, element, attrs) {
-			// var num = scope.$eval(attrs.px);
-			// console.log('number: '+num);
-			// var w = angular.element($window);
-			// console.log('width: '+ w.width());
-			// var subs = w.width() - num - 64;
-			// angular.element(element).css('width', subs);
-			// angular.element(element).on('resize', function(){
-			// 	angular.element(element).css('width', subs);
-			// });
-			// console.log(scope);
-			// console.log(angular.element(element).controller());
-			// scope.callback({ data: num });
-
-
 			$rootScope.$watch('windowSize.width', function(newVal, oldVal){
 				console.log('newVal', newVal);
 				console.log('oldVal', oldVal);
+				angular.element(element).css('width', newVal - scope.num() - 15);
 				// console.log('$window', $window);
 			});
 			angular.element($window).bind('resize', function(){
@@ -37,14 +24,10 @@ angular.module('Directives', [])
 			console.log(scope.num());
 			scope.callback({ data: scope.num() });
 			/**
-			
 				TODO:
-				- RECALCULAR Width para hacer el resize
-				- Second todo item
+				- cambiar windowSize.width en el watch por un attr variable			
+			**/
 			
-				**/
-
-
 		}
 	};
 }]);
