579a9207cecb2d68f7d33995b05b9ed2f2ceac51
Derrick Roccka <derrickroccka@gmail.com>
2014-03-13 17:55:02 +0100

    Video statistics working. Video and data changes on clicking on the thumbnail. Video section almost ended.

--- app/scripts/controllers/videos.js
+++ app/scripts/controllers/videos.js
@@ -1,7 +1,19 @@
 'use strict';
 
 angular.module('VideosModule', [])
-  .controller('VideosCtrl', [ '$scope', 'YoutubeService', function ($scope, YoutubeService) {
-    $scope.section = 'videos';
-    $scope.videoList = YoutubeService.getVideoList();
-  }]);
+.controller('VideosCtrl', [ '$scope', '$sce', 'YoutubeService', function ($scope, $sce, YoutubeService) {
+	var baseVideoURL = 'http://www.youtube.com/embed/';
+	var baseVideoId = '_Ah2nTHgMCI';
+	$scope.section = 'videos';
+	$scope.videoTitle = '"Putas Asesinas" de Roberto Bolaño';
+	$scope.videoURL = $sce.trustAsResourceUrl(baseVideoURL+'_Ah2nTHgMCI');
+	$scope.videoDesc = 'Vídeo promocional de l\'adaptació teatral del conte "Putas Asesinas" de Roberto Bolaño.';
+	$scope.videoList = YoutubeService.getVideoList();
+	$scope.videoStatistics = YoutubeService.getVideoStatistics(baseVideoId);
+	$scope.videoChange = function (title, id, desc) {
+		$scope.videoTitle = title;
+		$scope.videoURL = $sce.trustAsResourceUrl(baseVideoURL+id);
+		$scope.videoDesc = desc;
+		$scope.videoStatistics = YoutubeService.getVideoStatistics(id);
+	};
+}]);
--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -1,22 +1,39 @@
 'use strict';
 
 angular.module('Services', [])
-	.factory('YoutubeService', [ '$http', function($http) {
-		var url = 'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUtL3CrHPV6qHBaK1IUBoBIg&key=AIzaSyD-OH-663ZPQs9jWyi8rYWdXzK3P4Xtn9U&callback=JSON_CALLBACK&part=snippet&fields=items(snippet(description,thumbnails,title,resourceId))';
-		var videoList = [];
-		$http.jsonp(url)
-		.success(function(data){
-			console.log(data);
-			angular.forEach(data.items, function(item){
-				videoList.push(item);
+.factory('YoutubeService', [ '$http', function($http) {
+	return {
+		getVideoList: function() {
+			var url = 'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUtL3CrHPV6qHBaK1IUBoBIg&key=AIzaSyD-OH-663ZPQs9jWyi8rYWdXzK3P4Xtn9U&callback=JSON_CALLBACK&part=snippet&fields=items(snippet(description,thumbnails,title,resourceId))';
+			var videoList = [];
+			$http.jsonp(url)
+			.success(function(data){
+				console.log(data);
+				angular.forEach(data.items, function(item){
+					videoList.push(item);
+				});
+			})
+			.error(function(err){
+				throw new Error(err);
 			});
-		})
-		.error(function(err){
-			throw new Error(err);
-		});
-		return {
-			getVideoList: function() {
-				return videoList;
-			}
-		};
-	}]);
+			return videoList;
+		},
+
+		getVideoStatistics: function(id){
+			var statisticsURL = 'https://www.googleapis.com/youtube/v3/videos?id='+id+'&key=AIzaSyD-OH-663ZPQs9jWyi8rYWdXzK3P4Xtn9U&callback=JSON_CALLBACK&part=statistics&fields=items(statistics)';
+			var statistics = [];
+			$http.jsonp(statisticsURL)
+			.success(function(data){
+				console.log(data);
+				angular.forEach(data.items, function(item){
+					statistics.push(item);
+				});
+			})
+			.error(function(err){
+				throw new Error(err);
+			});
+
+			return statistics;
+		}
+	};
+}]);
