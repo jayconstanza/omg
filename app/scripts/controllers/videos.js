2bde8db0454b7cb9f9204f0df0f845f1fd9a206d
Derrick Roccka <derrickroccka@gmail.com>
2014-03-26 00:01:24 +0100

    x

--- app/scripts/controllers/videos.js
+++ app/scripts/controllers/videos.js
@@ -11,7 +11,7 @@ angular.module('VideosModule', [])
 	$scope.videoDesc = 'Vídeo promocional de l\'adaptació teatral del conte "Putas Asesinas" de Roberto Bolaño.';
 	$scope.videoList = YoutubeService.getVideoList();
 	$scope.videoStatistics = YoutubeService.getVideoStatistics(baseVideoId);
-
+	$scope.nowplaying = 'http://twitter.com/home?status=¡Hey!%20Echa%20un%20vistazo%20a%20'+$scope.videoTitle+'%20de%20OMartinGual+' + $scope.videoURL;
 	$scope.videoChange = function (title, id, desc) {
 		$scope.videoTitle = title;
 		$scope.videoURL = $sce.trustAsResourceUrl(baseVideoURL+id);
