6988434287ad4469ddff1289fc1a3b73875ab991
Derrick Roccka <derrickroccka@gmail.com>
2014-04-22 21:39:54 +0200

    changed title to Omartingual

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -4,5 +4,6 @@ angular.module('HomeModule', [])
 .controller('HomeCtrl', [ '$scope', function ($scope) {
 	$scope.num = 128;
 	$scope.section = 'home';
+	$scope.title = 'Omartingual';
 	$scope.tagline = 'Realizador de audiovisuales y espectáculos';
 }]);
