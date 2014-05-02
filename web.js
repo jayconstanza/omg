a8b7a028a2c35e49e307f7b1dfa1b5e6a96b8fe5
Derrick Roccka <derrickroccka@gmail.com>
2014-05-02 12:02:59 +0200

    provisional. All get requests are redirected to the home page

--- web.js
+++ web.js
@@ -3,11 +3,14 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
- 
+
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip('' + __dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
 
-app.get('/fotos', function(req, res) {
-	res.sendfile('' + __dirname + '/dist/views/snapshots/snapshot__fotos.html'); // load the single view file (angular will handle the page changes on the front-end)
+// app.get('/fotos', function(req, res) {
+// 	res.sendfile('' + __dirname + '/dist/views/snapshots/snapshot__fotos.html'); // load the single view file (angular will handle the page changes on the front-end)
+// });
+app.get('*', function(req, res){
+	res.redirect('/');
 });
