75965a507b88899612023e6a437d74d398606cae
Derrick Roccka <derrickroccka@gmail.com>
2014-05-03 01:53:30 +0200

    mute video button added. Footer link in pink

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -7,6 +7,8 @@ angular.module('HomeModule', [])
 	$scope.title = 'Omartingual.com';
 	$scope.tagline = 'Realizador de audiovisuales y espectáculos';
 	$scope.onSize = 768;
+	$scope.videoMuted = false;
+	$scope.videoMutedIcon = 'glyphicon-volume-up glyphicon';
 	$scope.flipCircle = function(){
 		if($scope.flip){
 			$scope.klass = 'flipped';
@@ -14,5 +16,22 @@ angular.module('HomeModule', [])
 		else{
 			$scope.klass = '';
 		}
+	};
+	$scope.muteVideo = function() {
+		var elem = document.getElementById('home-video');
+		// window.alert($scope.videoMuted);
+		if($scope.videoMuted === false ){
+			$scope.videoMuted = !$scope.videoMuted;
+			angular.element(elem).prop('muted', !angular.element(elem).prop('muted'));
+			angular.element(elem).attr('volume', 0);
+			$scope.videoMutedIcon = 'glyphicon-volume-off glyphicon';
+		}
+		else{
+			$scope.videoMuted = !$scope.videoMuted;
+			angular.element(elem).prop('muted', !angular.element(elem).prop('muted'));
+			angular.element(elem).attr('volume', 1);
+			$scope.videoMutedIcon = 'glyphicon-volume-up glyphicon';
+		}
+		$scope.apply();
 	};
 }]);
